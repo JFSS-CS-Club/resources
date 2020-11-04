5f7f427bb2f34a1925f7adfe56c984d82e8456e5
Jason Huang <jasonhuang20035@gmail.com>
2020-11-04 16:16:25 -0500

    chore: add lighthouse config

---
+++ lighthouserc.js
@@ -0,0 +1,11 @@
+module.exports = {
+    ci: {
+        collect: {
+            staticDistDir: './public',
+            url: ['http://localhost/index.html'],
+        },
+        upload: {
+            target: 'temporary-public-storage',
+        },
+    },
+};
