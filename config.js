e249af189e2050b7d33e12db5a59d1c6e6e0e1b9
Jason Huang <jasonhuang20035@gmail.com>
2020-11-04 16:08:08 -0500

    feat: add PWA support

--- config.js
+++ config.js
@@ -39,26 +39,20 @@ const config = {
     title: 'JFSS CS Club Resources',
     description: 'Some resources for the JFSS CS Club',
     ogImage: null,
-    docsLocation: '',
-    favicon: '',
+    docsLocation: 'https://github.com/jfsscsclub/resources',
+    favicon: 'content/icon.png',
   },
   pwa: {
-    enabled: false, // disabling this will also remove the existing service worker.
+    enabled: true, // disabling this will also remove the existing service worker.
     manifest: {
       name: 'JFSS CS Club Resources',
       short_name: 'JFSS CS Resources',
       start_url: '/',
-      background_color: '#6b37bf',
-      theme_color: '#6b37bf',
+      background_color: '#001934',
+      theme_color: '#001934',
       display: 'standalone',
       crossOrigin: 'use-credentials',
-      icons: [
-        {
-          src: 'src/pwa-512.png',
-          sizes: `512x512`,
-          type: `image/png`,
-        },
-      ],
+      icon: 'content/icon.png',
     },
   },
 };
